6984caabe4f9daf78c2e80eaa68f3fbf62552b8b
RayhanaMoh <s202243580@kfupm.edu.sa>
2026-04-13 12:22:36 +0300

    Todo6

--- 7-1-mongodb/server.js
+++ 7-1-mongodb/server.js
@@ -225,3 +225,8 @@ async function updateStudent() {
 updateStudent();
 
 // delete document
+async function deleteStudent() {
+    await Student.deleteOne({ name: "Sara" });
+    console.log("✅ Deleted Sara");
+}
+deleteStudent();
